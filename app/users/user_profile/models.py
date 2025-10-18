414a5d70984b2ee40c3344947abe217559ad9748
Sereja <118850367+bazuly@users.noreply.github.com>
2025-10-18 20:12:27 +0300

    1

--- app/users/user_profile/models.py
+++ app/users/user_profile/models.py
@@ -23,12 +23,12 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True), primary_key=True, default=uuid4)
+        UUID(as_uuid=True), primary_key=True, default=uuid4
+    )
     email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
     full_name: Mapped[str] = mapped_column(String(255))
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
-    yandex_id: Mapped[str | None] = mapped_column(
-        String(100), unique=True)  # oauth2
+    yandex_id: Mapped[str | None] = mapped_column(String(100), unique=True)  # oauth2
 
     trips_as_organizer: Mapped[list["Trip"]] = relationship(
         back_populates="organizer", foreign_keys="Trip.organizer_id"
